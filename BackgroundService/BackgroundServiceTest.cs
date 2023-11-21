9deab5d40fa7f649b8116396277a1aef42f47145
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-21 16:27:12 +0500

    Background Service

---
+++ BackgroundService/BackgroundServiceTest.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace BackgroundService1
+{
+    public class BackgroundServiceTest : IHostedService
+    {
+
+        // BAckgroundService
+
+        //protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        //{
+        //    PeriodicTimer? period=new PeriodicTimer(TimeSpan.FromSeconds(2));
+
+        //    while (await period.WaitForNextTickAsync(stoppingToken))
+        //    {
+        //        Console.WriteLine("Dastur ishga tushdii");
+
+        //    }
+
+        //}
+
+
+        // IHosted Services
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BackgroundService/Program.cs
+++ BackgroundService/Program.cs
@@ -1,32 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using BackgroundService1;
 
-// Add services to the container.
+WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+builder.Services.AddHostedService<BackgroundServiceTest>();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+WebApplication? app = builder.Build();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+
+
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
